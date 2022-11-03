679fff28dfe69c4bec19212e8a8f5ecdafa485ad
Wellerson <areadev@outlook.com>
2022-11-03 12:29:12 -0300

    Mobile
    Terminei o modo mobile.

---
+++ script/script.js
@@ -0,0 +1,54 @@
+function abrirFechar01() {
+    let tema01 = document.querySelector('iframe#tema01')
+    console.log(tema01)
+
+    if(tema01.style.display == 'block') {
+        tema01.style.display = 'none'
+    } else {
+        tema01.style.display = 'block'
+    }
+}
+
+function abrirFechar02() {
+    let tema02 = document.querySelector('iframe#tema02')
+    console.log(tema02)
+
+    if(tema02.style.display == 'block') {
+        tema02.style.display = 'none'
+    } else {
+        tema02.style.display = 'block'
+    }
+}
+
+function abrirFechar03() {
+    let tema03 = document.querySelector('iframe#tema03')
+    console.log(tema03)
+
+    if(tema03.style.display == 'block') {
+        tema03.style.display = 'none'
+    } else {
+        tema03.style.display = 'block'
+    }
+}
+
+function abrirFechar04() {
+    let tema04 = document.querySelector('iframe#tema04')
+    console.log(tema04)
+
+    if(tema04.style.display == 'block') {
+        tema04.style.display = 'none'
+    } else {
+        tema04.style.display = 'block'
+    }
+}
+
+function abrirFechar05() {
+    let tema05 = document.querySelector('iframe#tema05')
+    console.log(tema05)
+
+    if(tema05.style.display == 'block') {
+        tema05.style.display = 'none'
+    } else {
+        tema05.style.display = 'block'
+    }
+}
