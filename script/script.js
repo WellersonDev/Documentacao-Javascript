c0a400d65a7891b8e77a9ca7f9ba1a3295950a5b
Wellerson <areadev@outlook.com>
2022-11-16 12:35:17 -0300

    Inputs e Iframes
    Ajustes nos inputs e iframes.

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+
+
 function abrirFechar01() {
     let tema01 = document.querySelector('iframe#tema01')
     console.log(tema01)
@@ -50,5 +52,47 @@ function abrirFechar05() {
         tema05.style.display = 'none'
     } else {
         tema05.style.display = 'block'
+    }
+}
+
+function troca01() {
+    let assunto01 = document.querySelector('iframe#assunto01')
+    let assunto02 = document.querySelector('iframe#assunto02')
+    let assunto03 = document.querySelector('iframe#assunto03')
+    let assunto04 = document.querySelector('iframe#assunto04')
+    let assunto05 = document.querySelector('iframe#assunto05')
+
+    if(window.innerWidth >= 768) {
+        assunto01.removeAttribute('class')
+        assunto02.removeAttribute('class')
+        assunto03.removeAttribute('class')
+        assunto04.removeAttribute('class')
+        assunto05.removeAttribute('class')
+        assunto01.setAttribute('class', 'indice1')
+        assunto02.setAttribute('class', 'indice0')
+        assunto03.setAttribute('class', 'indice0')
+        assunto04.setAttribute('class', 'indice0')
+        assunto05.setAttribute('class', 'indice0')
+    }
+}
+
+function troca02() {
+    let assunto01 = document.querySelector('iframe#assunto01')
+    let assunto02 = document.querySelector('iframe#assunto02')
+    let assunto03 = document.querySelector('iframe#assunto03')
+    let assunto04 = document.querySelector('iframe#assunto04')
+    let assunto05 = document.querySelector('iframe#assunto05')
+
+    if(window.innerWidth >= 768) {
+        assunto01.removeAttribute('class')
+        assunto02.removeAttribute('class')
+        assunto03.removeAttribute('class')
+        assunto04.removeAttribute('class')
+        assunto05.removeAttribute('class')
+        assunto01.setAttribute('class', 'indice0')
+        assunto02.setAttribute('class', 'indice1')
+        assunto03.setAttribute('class', 'indice0')
+        assunto04.setAttribute('class', 'indice0')
+        assunto05.setAttribute('class', 'indice0')
     }
 }
